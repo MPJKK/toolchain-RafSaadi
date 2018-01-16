1a97fe430be3bcf14596035da69a905764b28b1f
Raouf Fahandezh Saadi <raouff@ety-a2123b-21.edu.wks.metropolia.fi>
2018-01-16 15:20:29 +0200

    -a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MainComponent } from './main/main.component';
+import { SiderbarComponent } from './siderbar/siderbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    MainComponent,
+    SiderbarComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
